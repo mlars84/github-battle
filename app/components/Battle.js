324eee14316bd938c6b0181e2e956528a4f0d11b
Matt Larson <matt.a.larson@gmail.com>
2017-09-18 15:30:00 -0500

    working on battle component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
 
 class Battle extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null
+    }
+    this.handleSumbit = this.handleSumbit.bind()
+  }
+  handleSumbit(id, username) {
+    this.setState(function() {
+      let newState = {}
+      newtate[id + 'Name'] = username
+      newState[id + 'Image'] = 'https://www.github.com/' + username + '.png?size=200'
+      return newState
+    })
+  }
   render() {
     return (
       <div className='battle-container'>
         <div>
           <h1>
             Battle page
-          </h1>  
+          </h1>
         </div>
       </div>
     )
